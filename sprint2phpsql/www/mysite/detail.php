136dcd11fb66f542f452eb9630a893f3c85663b7
Alejandro <aregueiror22@fpcoruna.afundacion.org>
2023-10-19 10:56:32 +0200

    Ejercicio 5 hecho mas añadi un = a value que faltaba en el formulario

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -31,7 +31,7 @@
 	<p>Deja un nuevo comentario:</p>
 	<form action="/comment.php" method="post">
 	   <textarea rows="4" cols="50" name="new_comment"></textarea><br>
-	   <input type="hidden" name="libro_id" value "<?php echo $libro_id; ?>">
+	   <input type="hidden" name="libro_id" value= "<?php echo $libro_id; ?>">
 	   <input type="submit" value="comentar">
 	</form>
    </body>
